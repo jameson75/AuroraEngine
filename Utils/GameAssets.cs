010437706ae2e51a8254b8e0c5ccbcd82d8129a9
meganova75 <5134768+meganova75@users.noreply.github.com>
2013-03-01 01:02:50 +0000

    started config implementation.

--- Utils/GameAssets.cs
+++ Utils/GameAssets.cs
@@ -5,18 +5,30 @@ using SharpDX.XAudio2;
 using SharpDX.Direct3D11;
 using SharpDX.DirectInput;
 using CipherPark.AngelJacket.Core;
+using CipherPark.AngelJacket.Core.World;
+using CipherPark.AngelJacket.Core.World.ParticleSystem;
+using CipherPark.AngelJacket.Core.World.Geometry;
+using CipherPark.AngelJacket.Core.World.Scene;
 
 namespace CipherPark.AngelJacket.Core.Utils
 {
     public class GameAssets
     {
         private IGameApp _game = null;
-
-        public IGameApp Game { get { return _game; } }
+        private Dictionary<string, Camera> _cameras = new Dictionary<string, Camera>();
+        private Dictionary<string, SceneNode> _sceneNodes = new Dictionary<string, SceneNode>();
 
         public GameAssets(IGameApp game)
         {
             _game = game;
-        }       
+        }
+
+        public IGameApp Game { get { return _game; } }
+        
+        public Model Player1 { get; set; }
+
+        public Dictionary<string, Camera> Cameras { get { return _cameras; } }
+
+        public Dictionary<string, SceneNode> SceneNodes { get { return _sceneNodes; } }
     }
 }
